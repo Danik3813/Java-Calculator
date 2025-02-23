892f7fb39c75bbaa793f7998f061195a9b38e17e
Danil Kupcov <danilkup23@yandex.ru>
2025-02-23 19:36:36 +0400

    Create repository

---
+++ calculator/src/Main.java
@@ -0,0 +1,9 @@
+import gui.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        CalculatorGUI calculatorGUI = new CalculatorGUI("Калькулятор");
+        calculatorGUI.initializeGUI(500, 500);
+    }
+}
+    
